cd400e35b8f187831d7e8f418b6afedbe713271e
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-03-08 00:23:32 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,15 +7,6 @@ class ChattyStudent
   end
   
   def raise_hand
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
+    10.times {super}
   end
   end
